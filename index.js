8d3e420e0b300b038f8b6db4058630210666fa24
Dustin Doyle <debodoyle@gmail.com>
2018-04-07 10:46:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const subtract = (a, b) => {
   return a - b;
 }
 
-function divide() {
-  
+function divide(a, b) {
+  return a / b;
 }
 
-const multiply = () => {
-  
+const multiply = (a, b) => {
+  return a * b;
 }
 
 add(5, 4);
 subtract(3, 2);
+divide(6, 2);
+multiply(5, 3);
